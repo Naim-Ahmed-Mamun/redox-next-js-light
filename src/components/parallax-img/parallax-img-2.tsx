d96e2f2d536fba7ae2c721463ec6f4c0a7fc0011
Naim Ahmed <naimahmod852@gmail.com>
2025-08-20 21:46:22 +0600

    updated animation

--- src/components/parallax-img/parallax-img-2.tsx
+++ src/components/parallax-img/parallax-img-2.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import Image from "next/image";
+// import gallery_img from "@/assets/imgs/gallery/image-52.webp";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 export default function ParallaxImgTwo() {
-  gsap.registerPlugin(ScrollTrigger);
+
   return (
-    <div className="image-wrapper parallax-view fade-anim">
+    <div className="image-wrapper parallax-view">
       <img
         className="w-100"
         src="/assets/imgs/gallery/image-52.webp"
